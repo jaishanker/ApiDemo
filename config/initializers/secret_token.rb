b512d6dc9e39942f0ae1dfd661c693c36a9529d9
jaishanker <jaishanker123@gmail.com>
2013-05-20 17:09:24 +0530

    api demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApiDemo::Application.config.secret_token = '7edaa2ea768943657eabce7e25cd7b882302943f8375d3e4f8e5f191bb4ca69a8b3e14a250dae835ac406fee9b220e9dbe0cd41bafb226bc64303906c2cc4e26'
